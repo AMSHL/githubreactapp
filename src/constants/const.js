bc578c71bfd5c7bbbf1e7999d03ed927c49b63bf
Anatoly Shlom <a.m.shlom@gmail.com>
2020-03-28 03:20:04 +0300

    cleanup

--- src/constants/const.js
+++ src/constants/const.js
@@ -5,4 +5,4 @@ export const PATH_REPO='/repo';
 
 export const API_URL = 'https://api.github.com/graphql';
 export const INITIAL_USER = 'AMSHL';
-export const TOKEN = 'ff83d791826fe3664758dc62e10e2d91672125ec';
+export const TOKEN = '';
